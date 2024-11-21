5e7270378c972be11642d71843fd68780c502676
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-11-21 11:57:01 +0530

    install terraform aws_cli kubectl

---
+++ terraform-learn/install.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+# Install Terraform
+TF_VERSION=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].builds[].version' | egrep -v 'rc|beta|alpha' | tail -1)
+
+curl -LO "https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip"
+unzip terraform_${TF_VERSION}_linux_amd64.zip
+sudo mv -f terraform /usr/local/bin/
+rm terraform_${TF_VERSION}_linux_amd64.zip
+
+terraform init
+
+# # Install KubeCTL
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.3/2023-11-14/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm -rf kubectl
+
+# # Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip
+rm -rf aws
+
+aws configure
+
+terraform apply
+
+aws eks update-kubeconfig --region us-east-1 --name my-cluster-eks
+
+kubectl apply -f app.yaml
